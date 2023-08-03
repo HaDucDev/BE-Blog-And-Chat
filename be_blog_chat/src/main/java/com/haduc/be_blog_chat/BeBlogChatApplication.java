f63d6c7d8817dea2dabaceb038e7c85b381e8f53
HaDucDev <haductrung392000@gmail.com>
2023-08-03 08:17:10 +0700

    start

---
+++ be_blog_chat/src/main/java/com/haduc/be_blog_chat/BeBlogChatApplication.java
@@ -0,0 +1,14 @@
+package com.haduc.be_blog_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeBlogChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeBlogChatApplication.class, args);
+		System.out.println("start");
+	}
+
+}
---
+++ be_blog_chat/src/test/java/com/haduc/be_blog_chat/BeBlogChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haduc.be_blog_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeBlogChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
